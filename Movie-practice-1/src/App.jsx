2243c29278d00b59e5be04db7a03c642a56e5944
Johnathanyes <johnathanylee327@gmail.com>
2024-06-04 16:59:55 -0400

    initialize

---
+++ Movie-practice-1/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+const APIURL = "http://www.omdbapi.com/?apikey=3b59e656"
+
+
+
+function App() {
+  
+  useEffect
+
+  return (
+    <>
+      H1
+    </>
+  );
+}
+
+export default App
